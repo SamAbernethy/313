3f1ff5c53c5e81a188dbf0683e07832574f7c672
SamAbernethy <12sa92@queensu.ca>
2015-11-26 18:40:55 -0500

    Almost have DLA

--- main.c
+++ main.c
@@ -9,6 +9,9 @@
 // FUNCTIONS ARE IN MAIN.H
 // ******************************
 
+void checkifperimeter(int DLA_sites[][EDEN_MAX], int randomwalker, int randomwalkery, int dlaperimeterxvalues[], int dlaperimeteryvalues[]);
+void randomwalk(int DLA_sites[][EDEN_MAX], int randomwalkerx[], int randomwalkery[], int dlaperimeterxvalues[], int dlaperimeteryvalues[]);
+
 void output(int sites[][EDEN_MAX], FILE *fp_out, int middle);
 void initializegrid(int sites[][EDEN_MAX], int xdisp, int ydisp);
 void resetperimetervalues(int perimeterxvalues[], int perimeteryvalues[]);
@@ -18,48 +21,46 @@ int updateperimeters(int sites[][EDEN_MAX], int perimeterxvalues[], int perimete
 int numberofperimeters(int sites[][EDEN_MAX]);
 void massandradius(FILE *dimension_out, int sites[][EDEN_MAX]);
 
+
 int main(void)
 {
-    /*printf("Hello World!\n");
-    printf("This is the start of our project \n");
-    FILE *fp_out;
-    fp_out = fopen("cluster.dat", "w");
-    srand((unsigned)time(NULL));
-    int x[MAX_ITERATIONS];
-    int y[MAX_ITERATIONS];
-    int l = 0;
-    for (l = 0; l < MAX_ITERATIONS; l++) {
-        x[l] = 0;
-        y[l] = 0;
+    srand((unsigned)time(NULL)); // seeds the pseudorandom generator
+
+    // ************************************
+    // DLA CLUSTER
+    // ************************************
+
+    printf("This is the start of our random walk\n");
+
+    FILE *dla_out;
+    dla_out = fopen("dla.dat", "w");
+
+    int DLA_sites[EDEN_MAX][EDEN_MAX];
+    initializegrid(DLA_sites, middle, middle);
+    int dlaperimeterxvalues[EDEN_MAX];
+    int dlaperimeteryvalues[EDEN_MAX];
+
+    int randomwalkerx[MAX_ITERATIONS];
+    int randomwalkery[MAX_ITERATIONS];
+
+    for (i = 0; i < TRIALS; i++) {
+        resetperimetervalues(dlaperimeterxvalues, dlaperimeteryvalues);
+
+        int dlanumber = updateperimeters(DLA_sites, dlaperimeterxvalues, dlaperimeteryvalues);
+        printf("Number of perimeter sites: %d\n", dlanumber);
+
+        randomwalk(DLA_sites, randomwalkerx, randomwalkery, dlaperimeterxvalues, dlaperimeteryvalues);
     }
-    int tempx = 0;
-    int tempy = 0;
-    int walkiterations = 0;
-    for (walkiterations = 1; walkiterations < MAX_ITERATIONS; walkiterations++) {
-        output(fp_out, tempx, tempy);
-        int j = pseudorandom(4);
-        printf("Random integer is %d\n", j);
-        if (j == 0) {
-            x[walkiterations] = x[walkiterations-1] + 1;
-            printf("New x is %d \n", x[walkiterations]);
-            y[walkiterations] = y[walkiterations-1];
-        }
-        else if (j == 1) {
-            x[walkiterations] = x[walkiterations-1] - 1;
-            y[walkiterations] = y[walkiterations-1];
-        }
-        else if (j == 2) {
-            y[walkiterations] = y[walkiterations-1] + 1;
-            x[walkiterations] = x[walkiterations-1];
-        }
-        else {
-            y[walkiterations] = y[walkiterations-1] - 1;
-            x[walkiterations] = x[walkiterations-1];
-        }
-        tempx = x[walkiterations];
-        tempy = y[walkiterations];
-    }
-    */
+
+    output(DLA_sites, dla_out, middle);
+
+
+
+
+
+    // ****************************************
+    // START OF EDEN
+    // ****************************************
 
     /* psuedo code for eden
         take a 101 by 101 array and put a 1 at (50,50), 0s elsewhere
@@ -68,8 +69,7 @@ int main(void)
         generate random number from 0 to r-1 and make that 2 point a 1
         repeat!
       */
-
-    srand((unsigned)time(NULL));
+/*
     printf("Starting Eden Cluster Model: \n");
     FILE *eden_out;
     FILE *killer_out;
@@ -110,6 +110,6 @@ int main(void)
 
     // check the dimensionality of the eden cluster
     massandradius(dimension_out, sites);
-
+    */
     return 0;
 }
--- main.h
+++ main.h
@@ -1,10 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#define MAX_ITERATIONS 10
-#define EDEN_MAX 501 // dimensions of grid
+#define MAX_ITERATIONS 100000
+#define EDEN_MAX 501 // dimensions of grid for Eden
 #define STRINGLENGTH 25 // maximum number of characters in a string
-#define TRIALS 4000 // number of points for Eden
+#define TRIALS 100 // number of points for Eden
+#define DLA_TRIALS 1000 // number of points for DLA
 
 // *****************************************
 // VARIABLE DECLARATIONS
@@ -13,10 +14,61 @@ int i; // index, usually the x axis of array
 int j; // index, usually the y axis of array
 int middle = (EDEN_MAX - 1) / 2; // middle of the grid, gets subtracted to center at origin
 
-
 // ******************************************
 // FUNCTIONS
 // ******************************************
+
+void checkifperimeter(int DLA_sites[][EDEN_MAX], int randomwalkerx, int randomwalkery, int dlaperimeterxvalues[], int dlaperimeteryvalues[]) {
+    int j;
+    for (j = 0; j < EDEN_MAX; j++) {
+        if ((randomwalkerx == dlaperimeterxvalues[j]) && (randomwalkery == dlaperimeteryvalues[j])) {
+            DLA_sites[randomwalkerx][randomwalkery] = 1;
+        }
+    }
+    return;
+}
+
+void randomwalk(int DLA_sites[][EDEN_MAX], int randomwalkerx[], int randomwalkery[], int dlaperimeterxvalues[], int dlaperimeteryvalues[]) {
+    int i;
+    int x[MAX_ITERATIONS]; // x location for random walk
+    int y[MAX_ITERATIONS]; // y location for random walk
+
+    for (i = 0; i < MAX_ITERATIONS; i++) {
+        x[i] = 0;
+        y[i] = 0;
+        randomwalkerx[i] = 0;
+        randomwalkery[i] = 0;
+    }
+
+    for (i = 0; i < MAX_ITERATIONS; i++) {
+        int j = pseudorandom(4);
+        if (j == 0) {
+            x[i] = x[i-1] + 1;
+            y[i] = y[i-1];
+        }
+        if (j == 1) {
+            x[i] = x[i-1] - 1;
+            y[i] = y[i-1];
+        }
+        if (j == 2) {
+            x[i] = x[i-1];
+            y[i] = y[i-1] - 1;
+        }
+        if (j == 3) {
+            x[i] = x[i-1];
+            y[i] = y[i-1] + 1;
+        }
+        int tempx = x[i];
+        int tempy = y[i];
+        checkifperimeter(DLA_sites, tempx, tempy, dlaperimeterxvalues, dlaperimeteryvalues);
+    }
+    return;
+}
+
+// ********************************************
+// FUNCTIONS USED BY EDEN
+// ********************************************
+
 
 // initializes all points to be 0's, except a 1 at a given point
 void initializegrid(int sites[][EDEN_MAX], int xdisp, int ydisp) {
